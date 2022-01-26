dcb22b926ecdd57b96a086fd6a3fbdd8baff2190
LaupWing <loc_strict@hotmail.com>
2022-01-26 14:38:25 +0100

    Layout

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Layout from '../components/Layout'
 
 export default function Home() {
    return (
-      <div>
-         <h1>Products</h1>
-         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
-         </ul>
-      </div>
+      <Layout>
+         <div>
+            <h1>Products</h1>
+            <ul>
+               <li>Product 1</li>
+               <li>Product 2</li>
+               <li>Product 3</li>
+            </ul>
+         </div>
+      </Layout>
    )
 }
