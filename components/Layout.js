688d28f335c300b1e27f6ce81afd06a27335799c
LaupWing <loc_strict@hotmail.com>
2022-01-28 10:51:13 +0100

    8

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,6 @@
-import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Container, Toolbar, Typography, Link } from '@material-ui/core';
 import Head from 'next/head';
+import NextLink from 'next/link';
 import React from 'react';
 import useStyles from '../utils/styles';
 
@@ -13,9 +14,22 @@ const Layout = ({children}) => {
          </Head>
          <AppBar className={classes.navbar} position='static'>
             <Toolbar>
-               <Typography>
-                  amazona
-               </Typography>
+               <NextLink href={'/'} passHref>
+                  <Link>
+                     <Typography className={classes.brand}>
+                        amazona
+                     </Typography>
+                  </Link>
+               </NextLink>
+               <div className={classes.grow}></div>
+               <div>
+                  <NextLink href={'/cart'} passHref>
+                     <Link>Cart</Link>
+                  </NextLink>
+                  <NextLink href={'/login'} passHref>
+                     <Link>Login</Link>
+                  </NextLink>
+               </div>
             </Toolbar>
          </AppBar>
          <Container className={classes.main}>
