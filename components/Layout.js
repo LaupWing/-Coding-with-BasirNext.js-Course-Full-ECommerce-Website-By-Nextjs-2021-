25c3f9fa8d262a5c7f5381c7e68dde6bd05f80bd
LaupWing <loc_strict@hotmail.com>
2022-01-28 10:23:28 +0100

    5

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,27 @@
 import { AppBar, Container, Toolbar, Typography } from '@material-ui/core';
 import Head from 'next/head';
 import React from 'react';
+import useStyles from '../utils/styles';
 
 const Layout = ({children}) => {
+   const classes = useStyles()
+
    return (
       <div>
          <Head>
             <title>Next Amazona</title>
          </Head>
-         <AppBar position='static'>
+         <AppBar className={classes.navbar} position='static'>
             <Toolbar>
                <Typography>
                   amazona
                </Typography>
             </Toolbar>
          </AppBar>
-         <Container>
+         <Container className={classes.main}>
             {children}
          </Container>
-         <footer>
+         <footer className={classes.footer}>
             <Typography>
                All rights resevered. Next Amazona
             </Typography>
